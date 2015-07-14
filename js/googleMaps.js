f97cd17671bf6eecb3b018ca73cdaf047c26167d
DevRob <mail.robertmolnar@gmail.com>
2015-07-14 14:21:43 +0100

    new toys

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,37 +1,31 @@
 //google maps
-var canvas = document.getElementById("googleMap");
-canvas.style.width  = window.innerWidth + 'px';
-canvas.style.height = window.innerHeight + 'px';
-
-function initialize() {
-  var mapOptions = {
-    center:new google.maps.LatLng(52, -8),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP,
-    minZoom: 3,
-    maxZoom: 18,
-    panControl: false,
-    zoomControl: false,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        position: google.maps.ControlPosition.RIGHT_BOTTOM},
-    scaleControl: false,
-    streetViewControl: false,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-/*
-window.onload = function() {
+var map, placesList;
+iMhere = function() {
   var startPos;
   var geoOptions = {
      timeout: 10 * 1000
   }
 
   var geoSuccess = function(position) {
-    startPos = position;
-    alert("lat: " + startPos.coords.latitude, "Lan: " + startPos.coords.longitude);
+    function initialize() {
+      var pyrmont = new google.maps.LatLng(startPos.coords.latitude, startPos.coords.latitude);
+
+      map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: pyrmont,
+        zoom: 17
+      });
+
+      var request = {
+        location: pyrmont,
+        radius: 1000,
+        types: ['store']
+      };
+
+      placesList = document.getElementById('places');
+
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, callback);
+    }
   };
   var geoError = function(error) {
     console.log('Error occurred. Error code: ' + error.code);
@@ -41,7 +35,53 @@ window.onload = function() {
     //   2: position unavailable (error response from location provider)
     //   3: timed out
   };
-
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
 };
-*/
+
+function callback(results, status, pagination) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    return;
+  } else {
+    createMarkers(results);
+
+    if (pagination.hasNextPage) {
+      var moreButton = document.getElementById('more');
+
+      moreButton.disabled = false;
+
+      google.maps.event.addDomListenerOnce(moreButton, 'click',
+          function() {
+        moreButton.disabled = true;
+        pagination.nextPage();
+      });
+    }
+  }
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+
+    placesList.innerHTML += '<li>' + place.name + '</li>';
+
+    bounds.extend(place.geometry.location);
+  }
+  map.fitBounds(bounds);
+}
+
+google.maps.event.addDomListener(window, 'load', iMhere);
